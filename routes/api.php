e7d9c0d85b575ec418cb9254791b539b612adbad
A Công <acongkplpe@gmail.com>
2022-07-11 17:14:08 +0700

    Url student lesson teacher feedback

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\StudentController;
+use App\Http\Controllers\API\TeacherController;
+use App\Http\Controllers\API\LessonController;
+use App\Http\Controllers\API\FeedbackController;
+use App\Http\Controllers\API\RegistrationController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,4 +22,8 @@ use App\Http\Controllers\API\StudentController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/student',[StudentController::class,'index']);
+Route::get('/students',[StudentController::class,'index']);
+Route::get('/teachers',[TeacherController::class,'index']);
+Route::get('/registrations',[RegistrationController::class,'index']);
+Route::get('/feedback',[FeedbackController::class,'index']);
+Route::get('/lessons',[LessonController::class,'index']);
