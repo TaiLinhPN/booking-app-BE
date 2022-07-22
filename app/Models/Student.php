7718b95d2777f42c67b0e769073fc29662100e83
A Công <acongkplpe@gmail.com>
2022-07-22 18:45:13 +0700

    studen model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Models;
-
+ 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Laravel\Sanctum\HasApiTokens;
-class Student extends Model implements ShouldQueue
+class Student extends Model  
 {
     use HasApiTokens, HasFactory, Notifiable;
 
